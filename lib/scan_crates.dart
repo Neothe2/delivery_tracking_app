17bda0a4c643008ed555a48ad1248883baa8c621
Neothe2 <neo.j.jabin@gmail.com>
2024-04-10 14:25:20 +0530

    Minimum viable product finished.

--- lib/scan_crates.dart
+++ lib/scan_crates.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:native_barcode_scanner/barcode_scanner.dart';
 
 import 'delivery_batches.dart';
 
@@ -24,6 +26,7 @@ class _ScanCratesPageState extends State<ScanCratesPage> {
   List<Crate> scannedCrates = [];
   double progressBarValue = 0;
   String correctionText = 'Enter the Crate ID';
+  bool text = false;
 
   @override
   void initState() {
@@ -34,19 +37,21 @@ class _ScanCratesPageState extends State<ScanCratesPage> {
     });
   }
 
-  scan() {
-    String crateId = crateIdTextController.value.text;
+  scan(String text) {
+    String crateId = text;
     crateIdTextController.text = '';
     Crate? scannedCrate = getCrateByIdOrNull(crateId, remainingCrates);
     if (scannedCrate == null) {
       scannedCrate = getCrateByIdOrNull(crateId, scannedCrates);
       if (scannedCrate == null) {
         setState(() {
+          HapticFeedback.heavyImpact();
           correctionText = 'Incorrect Crate';
           return;
         });
       } else {
         setState(() {
+          HapticFeedback.heavyImpact();
           correctionText = 'Already Scanned';
           return;
         });
@@ -82,28 +87,71 @@ class _ScanCratesPageState extends State<ScanCratesPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 70.0, vertical: 10),
-              child: TextField(
-                controller: crateIdTextController,
-                decoration: InputDecoration(labelText: "Crate ID"),
-              ),
-            ),
             OutlinedButton(
                 onPressed: () {
-                  scan();
+                  setState(() {
+                    text = !text;
+                  });
                 },
-                child: const Text('Enter')),
-            const SizedBox(
-              height: 50,
-            ),
-            Card(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(
-                  correctionText,
-                  style: TextStyle(fontSize: 25),
+                child:
+                    Text(text ? 'Scan QR code instead' : 'Enter Text instead')),
+            (text)
+                ? Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 70.0, vertical: 10),
+                        child: TextField(
+                          controller: crateIdTextController,
+                          decoration: InputDecoration(labelText: "Crate ID"),
+                        ),
+                      ),
+                      OutlinedButton(
+                          onPressed: () {
+                            scan(crateIdTextController.value.text);
+                          },
+                          child: const Text('Enter')),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                    ],
+                  )
+                : SizedBox(
+                    height: 250,
+                    width: 250,
+                    child: Container(
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20)),
+                      child: BarcodeScannerWidget(
+                        onError: (e) {},
+                        onBarcodeDetected: (Barcode barcode) {
+                          HapticFeedback.vibrate();
+                          scan(barcode.value);
+                          if (remainingCrates.isNotEmpty) {
+                            BarcodeScanner.startScanner();
+                          }
+                        },
+                        onTextDetected: (text) {},
+                      ),
+                    ),
+                  ),
+            // OutlinedButton(
+            //     onPressed: () {
+            //       scan(crateIdTextController.value.text);
+            //     },
+            //     child: const Text('Enter')),
+
+            SizedBox(
+              width: 256,
+              child: Card(
+                shape: LinearBorder(),
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Text(
+                    correctionText,
+                    style: TextStyle(fontSize: 25),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
               ),
             ),
@@ -113,6 +161,7 @@ class _ScanCratesPageState extends State<ScanCratesPage> {
             Text(
               '${scannedCrates.length}/${widget.crateList.length}',
               style: TextStyle(fontSize: 40),
+              textAlign: TextAlign.center,
             ),
             Padding(
               padding: const EdgeInsets.all(8.0),
@@ -122,11 +171,14 @@ class _ScanCratesPageState extends State<ScanCratesPage> {
                       minHeight: 50,
                       borderRadius: BorderRadius.circular(15),
                     )
-                  : OutlinedButton(
-                      onPressed: () {
-                        widget.afterScanningFinished();
-                      },
-                      child: const Text('Finish')),
+                  : SizedBox(
+                      width: 100000000,
+                      child: OutlinedButton(
+                          onPressed: () {
+                            widget.afterScanningFinished();
+                          },
+                          child: const Text('Finish')),
+                    ),
             )
           ],
         ),
